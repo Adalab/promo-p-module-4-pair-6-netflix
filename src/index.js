f7e3bd024f02982f1056a15a8bbe1b6e53c40cc4
Alejandra Diaz <francys.diazurribarri2@gmail.com>
2022-05-12 14:12:00 +0200

    redo

--- src/index.js
+++ src/index.js
@@ -7,28 +7,7 @@ server.use(cors());
 server.use(express.json());
 
 // init express aplication
-const serverPort = 3001;
+const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
-});
-
-server.get('/movies', (req, res) => {
-  console.log('Ha pasao por aqui.');
-  res.send({
-    success: true,
-    movies: [
-      {
-        id: '1',
-        title: 'Gambita de dama',
-        gender: 'Drama',
-        image: 'https://via.placeholder.com/150',
-      },
-      {
-        id: '2',
-        title: 'Friends',
-        gender: 'Comedia',
-        image: 'https://via.placeholder.com/150',
-      },
-    ],
-  });
 });
