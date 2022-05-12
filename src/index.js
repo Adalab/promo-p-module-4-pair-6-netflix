8f40b0ba17d366a271f2c0392545d33973d64e54
Alejandra Diaz <francys.diazurribarri2@gmail.com>
2022-05-12 11:09:37 +0200

    adds endopoint 4000

--- src/index.js
+++ src/index.js
@@ -7,7 +7,28 @@ server.use(cors());
 server.use(express.json());
 
 // init express aplication
-const serverPort = 4000;
+const serverPort = 3001;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+  console.log('Ha pasao por aqui.');
+  res.send({
+    success: true,
+    movies: [
+      {
+        id: '1',
+        title: 'Gambita de dama',
+        gender: 'Drama',
+        image: 'https://via.placeholder.com/150',
+      },
+      {
+        id: '2',
+        title: 'Friends',
+        gender: 'Comedia',
+        image: 'https://via.placeholder.com/150',
+      },
+    ],
+  });
 });
