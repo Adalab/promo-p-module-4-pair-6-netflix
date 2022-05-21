1d4e8905bda826569f3ed2aedc0855182165709a
Alejandra Diaz <francys.diazurribarri2@gmail.com>
2022-05-21 18:38:32 +0200

    adds filter gender

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Importamos librerias
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 //const movies = require('./data/movies.json');
-const Database = require("better-sqlite3");
+const Database = require('better-sqlite3');
 
 // create and config server, configuración de ExpressJS para APIS:
 const app = express();
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // Congiguración de ExpressJS para dinámicos
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Configuración de la base de datos SQLite:
-const db = new Database("./src/db/database.db", { verbose: console.log });
+const db = new Database('./src/db/database.db', { verbose: console.log });
 
 // Arrancar el servidor:
 const serverPort = 4000;
@@ -23,50 +23,50 @@ app.listen(serverPort, () => {
 
 //endpoint movies
 
+// app.get('/movies', (req, res) => {
+//   const query = db.prepare(`SELECT * FROM movies  ORDER BY  title `);
+//   const movieList = query.all();
+//   res.json({
+//     success: true,
+//     movies: movieList,
+//   });
+// });
+
+//gender filter
 app.get('/movies', (req, res) => {
-  const query = db.prepare(`SELECT * FROM movies  ORDER BY  title `);
-  const movieList = query.all();
+  let movies = [];
+  const genderParam = req.query.gender;
+  if (genderParam === '') {
+    const query = db.prepare(`SELECT * FROM movies`);
+    movies = query.all();
+  } else {
+    const query = db.prepare(`SELECT * FROM movies WHERE gender= ?`);
+    movies = query.all(genderParam);
+  }
   res.json({
     success: true,
-    movies: movieList,
+    movies: movies,
   });
 });
 
-//gender filter
-//app.get("/movies", (req, res) => {
-  //let movies = [];
-  //const genderParam = req.query.gender;
-  //if (genderParam === "") {
-    //const query = db.prepare(`SELECT * FROM movies`);
-    //movies = query.all();
-  //} else {
-    //const query = db.prepare(`SELECT * FROM movies WHERE gender= ?`);
-    //movies = query.all(genderParam);
-  //}
-  //res.json({
-    //success: true,
-    //movies: movies,
-  //});
-//});
-
 //servidor dinámico
 
-app.get("/movie/:movieId", (req, res) => {
+app.get('/movie/:movieId', (req, res) => {
   const query = db.prepare(`SELECT * FROM movies WHERE id = ?`);
   const movie = query.get(req.params.movieId);
   console.log(movie);
 
-  res.render("movies", movie);
+  res.render('movies', movie);
 });
 
 //servidor estático
-const staticServerPath = "./src/public-react";
+const staticServerPath = './src/public-react';
 app.use(express.static(staticServerPath));
 
-const staticServerPathImgs = "./src/public-images";
+const staticServerPathImgs = './src/public-images';
 app.use(express.static(staticServerPathImgs));
 
 // Crear servicio estático para los estilos
 
-const staticServerPathStyles = ".src/web/src/stylesheets";
+const staticServerPathStyles = './src/public-react/static/css';
 app.use(express.static(staticServerPathStyles));
