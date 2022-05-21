545f8793ede6ec304e1197b61a081458e714c08d
Iris Mireya <irismzb_86@hotmail.com>
2022-05-21 13:22:19 +0200

    add movies

--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,9 +1,6 @@
-// login
-
 const getMoviesFromApi = () => {
-  console.log('Se están pidiendo las películas de la app');
-  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('http://localhost:4000/movies')
+  console.log("Se están pidiendo las películas de la app");
+  return fetch("http://localhost:4000/movies")
     .then((response) => response.json())
     .then((data) => {
       return data;
